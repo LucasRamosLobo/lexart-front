5b481ba1f48eae71bdb7099ec9a2e344975e91ed
LucasRamosLobo <lucasramoslobo83@gmail.com>
2024-03-07 00:05:07 -0300

    feat: altera token na requisição

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -7,18 +7,27 @@ const ProductsPage = () => {
   useEffect(() => {
 
     const fetchProducts = async () => {
-      try {
-        const response = await fetch('https://lexart-back-ecru.vercel.app/api/products');
-        if (response.ok) {
-          const data = await response.json();
-          setProducts(data);
-        } else {
-          console.error('Failed to fetch products:', response.status);
+        try {
+         
+          const token = localStorage.getItem('token');
+      
+          const response = await fetch('https://lexart-back-ecru.vercel.app/api/products', {
+            headers: {
+              'Authorization': `${token}`,
+              'Content-Type': 'application/json',
+            },
+          });
+      
+          if (response.ok) {
+            const data = await response.json();
+            setProducts(data);
+          } else {
+            console.error('Failed to fetch products:', response.status);
+          }
+        } catch (error) {
+          console.error('Error fetching products:', error);
         }
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
-    };
+      };
 
     fetchProducts();
   }, []);
